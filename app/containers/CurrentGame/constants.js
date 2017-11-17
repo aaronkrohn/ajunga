735e6f36a4b10edbd707f65cfa276d75b2454024
Aaron Krohn <aaron@filtered.com>
2017-11-17 22:22:05 +0000

    Add server indicator

--- app/containers/CurrentGame/constants.js
+++ app/containers/CurrentGame/constants.js
@@ -6,3 +6,4 @@
 
 export const ADD_FINAL_SCORE = 'app/CurrentGame/ADD_FINAL_SCORE';
 export const INCREMENT_GAME_WON = 'app/CurrentGame/INCREMENT_GAME_WON';
+export const CHECK_SERVER = 'app/CurrentGame/CHECK_SERVER';
